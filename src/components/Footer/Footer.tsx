3c016e69993c564e42929452119d2dc7e54d344f
Isa <isarahatbekov250@gmail.com>
2025-11-15 17:55:21 +0600

    foote, header was creaated and some stile

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,95 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+	return (
+		<div className={styles.Footer}>
+			<div className={`${styles.container} container`}>
+				<nav className={styles.nav}>
+					<ul className={styles.list}>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+					</ul>
+					<ul className={styles.list}>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+					</ul>
+					<ul className={styles.list}>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+						<li className={styles.item}>
+							<a className={styles.link} href='#'>
+								links
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	)
+}
+
+export default Footer
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
+import Footer from './components/Footer/Footer.tsx'
+import Header from './components/Header/Header.tsx'
+import './styles/global.scss'
+import './styles/reset.scss'
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<Header />
+		<main>
+			<App />
+		</main>
+		<Footer />
+	</StrictMode>
 )
