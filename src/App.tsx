731163d8119a08cbff96d5142bcaf031cff52f3b
Isa <isarahatbekov250@gmail.com>
2025-12-08 22:19:05 +0600

    some

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './features/Home/Home'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' element={<Home />} />
+				<Route path='/regis' element={<Register />} />
+				<Route path='/login' element={<Login />} />
 			</Routes>
 		</BrowserRouter>
 	)
---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,11 @@
+import styles from './Register.module.scss'
+
+const Register = () => {
+	return (
+		<div className={styles.Register}>
+			<h1>Registration</h1>
+		</div>
+	)
+}
+
+export default Register
