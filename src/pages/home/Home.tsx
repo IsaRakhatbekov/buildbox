1d834225e85e1e98bd44d8278dffa5c07eefa51a
Oscar <oscarzhantaev@gmail.com>
2025-12-09 12:37:59 +0600

    Update 09.12.2025

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,9 @@ import styles from './Home.module.scss'
 const Home = () => {
 	return (
 		<div className='container'>
-			<div className={styles.Home}>Home page</div>
+			<div className={styles.Home}>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio vero aut repudiandae facere in modi repellat, error magni minus culpa at aliquam deleniti enim labore ad ex quaerat perferendis totam?
+			</div>
 		</div>
 	)
 }
